7dfbe847f5f4ecebb158bf775c0fdd7a8cb6f0e0
shaddoxac <shaddoxac@gmail.com>
2015-04-28 20:21:04 -0500

    Rewrote Test for cleanliness

--- src/tests/ParserTests.java
+++ src/tests/ParserTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import URL.Parser;
 import model.Item;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,34 +11,57 @@ import static org.junit.Assert.assertTrue;
 public class ParserTests {
     private Parser parser = new Parser();
     private Item item;
-    private String testURL = "http://www.amazon.com/gp/product/B00IOY8XWQ/ref=s9_psimh_gw_p349_d0_i2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-1&pf_rd_r=1GFJPB3F15ZGZ6GGH4AV&pf_rd_t=36701&pf_rd_p=2079475182&pf_rd_i=desktop";
-    private String testPrice = "$199.00";
-    private String testTitle = "Kindle Voyage, 6\" High-Resolution Display (300 ppi) with Adaptive Built-in Light, PagePress Sensors, Wi-Fi - Includes Special Offers";
+    private Item test;
+    private Item book;
+    private Item game;
+    private Item backpack;
 
-    private String testBookURL = "http://www.amazon.com/Memory-Amos-Decker-David-Baldacci/dp/1455559822/ref=sr_1_1?s=books&ie=UTF8&qid=1429838562&sr=1-1&keywords=book";
-    private String bookTitle = "Memory Man (Amos Decker series)";
-    private String bookPrice = "$14.67";
+    @Before
+    public void initialize() {
+        test = new Item("http://www.amazon.com/gp/product/B00IOY8XWQ/ref=s9_psimh_gw_p349_d0_i2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-1&pf_rd_r=1GFJPB3F15ZGZ6GGH4AV&pf_rd_t=36701&pf_rd_p=2079475182&pf_rd_i=desktop");
+        test.price = "$199.00";
+        test.title = "Kindle Voyage, 6\" High-Resolution Display (300 ppi) with Adaptive Built-in Light, PagePress Sensors, Wi-Fi - Includes Special Offers";
+        test.shipping = "1";
 
-    private String gameURL = "http://www.amazon.com/Bloodborne-PlayStation-4/dp/B00KVR4HEC/ref=sr_1_1?ie=UTF8&qid=1429839651&sr=8-1&keywords=bloodborne";
-    private String gameTitle = "Bloodborne";
-    private String gamePrice = "$59.02";
+        book = new Item("http://www.amazon.com/Memory-Amos-Decker-David-Baldacci/dp/1455559822/ref=sr_1_1?s=books&ie=UTF8&qid=1429838562&sr=1-1&keywords=book");
+        book.price = "$14.67";
+        book.title = "Memory Man (Amos Decker series)";
+        book.shipping = "0";
+
+        game = new Item("http://www.amazon.com/Bloodborne-PlayStation-4/dp/B00KVR4HEC/ref=sr_1_1?ie=UTF8&qid=1429839651&sr=8-1&keywords=bloodborne");
+        game.price = "$59.02";
+        game.title = "Bloodborne";
+        game.shipping = "1";
+
+        backpack = new Item("http://www.amazon.com/JanSport-Superbreak-Classic-Backpack-Black/dp/B0007QCQGI/ref=sr_1_1?ie=UTF8&qid=1430147820&sr=8-1&keywords=backpack");
+        backpack.price = "$26.88";
+        backpack.title = "Classic SuperBreak Backpack";
+        backpack.shipping = "1";
+    }
+
 
     @Test
     public void testPrint() {
-        item = parser.parse(testURL);
-        checkItem(testTitle, testPrice, "1");
+        item = parser.parse(test.url);
+        checkItem(test);
     }
 
     @Test
     public void testBookURL() {
-        item = parser.parse(testBookURL);
-        checkItem(bookTitle, bookPrice, "0");
+        item = parser.parse(book.url);
+        checkItem(book);
     }
 
     @Test
     public void testGameURL() {
-        item = parser.parse(gameURL);
-        checkItem(gameTitle, gamePrice, "1");
+        item = parser.parse(game.url);
+        checkItem(game);
+    }
+
+    @Test
+    public void testBackpackURL() {
+        item = parser.parse(backpack.url);
+        printItemValues(item);
     }
 
 
@@ -47,10 +71,10 @@ public class ParserTests {
         System.out.println(item.shipping);
     }
 
-    private void checkItem(String title, String price, String shipping) {
-        assertEquals(title, item.title);
-        assertEquals(price, item.price);
-        assertTrue(item.shipping.equals(shipping));
+    private void checkItem(Item other) {
+        assertEquals(other.title, item.title);
+        assertEquals(other.price, item.price);
+        assertEquals(other.shipping, item.shipping);
         assertTrue(item.hasPrice());
         assertTrue(item.hasTitle());
     }
